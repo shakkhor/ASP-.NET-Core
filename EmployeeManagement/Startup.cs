cdd559261bb4e7d9b7f903d7ea6b231655668a29
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2020-09-02 10:10:30 +0600

    wwwroot added

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,8 +13,14 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private IConfiguration _config;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -32,7 +39,7 @@ namespace EmployeeManagement
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                    await context.Response.WriteAsync(_config["NewConfig"]);
                 });
             });
         }
